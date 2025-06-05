7f350eaee96248102e227fbfb53959d2d92338f4
Dave Smith <d@smiff.co.uk>
2025-06-05 09:23:01 +0100

    Integrate Google authentication using Passport.js

--- server.js
+++ server.js
@@ -1,7 +1,30 @@
 import express from 'express';
+import passport from 'passport';
+import { Strategy as GoogleStrategy } from 'passport-google-oauth20';
 
 const app = express();
 const port = 3000;
+
+passport.use(new GoogleStrategy({
+  clientID: 'YOUR_GOOGLE_CLIENT_ID',
+  clientSecret: 'YOUR_GOOGLE_CLIENT_SECRET',
+  callbackURL: 'http://localhost:3000/auth/google/callback'
+}, (accessToken, refreshToken, profile, done) => {
+  // Here you would find or create a user in your database
+  return done(null, profile);
+}));
+
+app.use(passport.initialize());
+
+app.get('/auth/google', passport.authenticate('google', {
+  scope: ['https://www.googleapis.com/auth/plus.login']
+}));
+
+app.get('/auth/google/callback', passport.authenticate('google', {
+  failureRedirect: '/login'
+}), (req, res) => {
+  res.redirect('/');
+});
 
 app.get('/hello', (req, res) => {
   res.send('hello');
